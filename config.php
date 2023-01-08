8fcd83d916a1e8076afbe06c2ffe46c9c1a2c0e2
hudapratamaas <hudapratama750@gmail.com>
2023-01-08 13:05:09 +0700

    commit
    Website Sistem Informasi Geografis Lokasi Objek Wisata di Kab. Brebes

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+//include dirname(__FILE__) . '/../config.php';
+//$host = "localhost";
+//$user = "root";
+//$pass = "";
+//$name = "gis";
+$config["server"]='localhost';
+$config["username"]='root';
+$config["password"]='';
+$config["database_name"]='tugasakhir_gis';
+
+//$koneksi = mysqli_connect($host, $user, $pass,$name) or die("Koneksi ke database gagal!");
